6b9add62faabcc9ad4aa00ace5bc53a25dc6c1a2
Kaustubh Umbarkar <kaustubhumbarkar36@gmail.com>
2024-02-13 11:12:15 +0530

    Routes injected

---
+++ src/components/About/About.jsx
@@ -0,0 +1,28 @@
+export default function About() {
+    return (
+        <div className="py-16 bg-white">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img
+                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            alt="image"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                            Kaustubh Umbarkar
+                        </h2>
+                        <p className="mt-6 text-gray-600">
+                             I have experience with other technologies such as HTML,Tailwind CSS, Redux. My proficiency in these technologies has helped me build complex web applications that have received accolades from peers.
+Furthermore, I have a strong background in Object-Oriented Programming, which has helped me to write clean, maintainable, and efficient code. , which will enable me to contribute to the success of your team.
+                        </p>
+                        <p className="mt-4 text-gray-600">
+                            Since mid-2020, I have been working extensively with JavaScript, and I have gained proficiency in various JavaScript frameworks such as ReactJS, AngularJS, and NodeJS.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
